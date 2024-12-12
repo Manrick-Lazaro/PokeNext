9ee693c8ae8ea50bb96ed305bd3cac5341cd5a42
Manrick Lazaro <manrick.lazaro@escolar.ifrn.edu.br>
2024-12-12 20:48:44 -0300

    implementa o css do footer.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,8 @@
+import styles from "../styles/footer.module.css";
+
 export function Footer(): JSX.Element {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>
         <span>PokeNext</span> &copy; 2024
       </p>
